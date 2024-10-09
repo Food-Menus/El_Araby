6ebc3d5689a6f31fe3a344ee983a128f9965c6a6
K A I S E R <153696501+KAISSOR@users.noreply.github.com>
2024-10-09 06:53:28 +0300

    update

--- Back-end/Mashwiat.js
+++ Back-end/Mashwiat.js
@@ -4,24 +4,74 @@ fetch('./../../Data-base/Mashwiat.json')
 
     document.getElementById('Mashwiat_type_1').textContent = data.Mashwiat_type_1;
     document.getElementById('Mashwiat_prise_1').textContent = data.Mashwiat_prise_1;
+    document.getElementById('Mashwiat_type_1_1').textContent = data.Mashwiat_type_1_1;
+    document.getElementById('Mashwiat_prise_1_1').textContent = data.Mashwiat_prise_1_1;
+    document.getElementById('Mashwiat_type_1_2').textContent = data.Mashwiat_type_1_2;
+    document.getElementById('Mashwiat_prise_1_2').textContent = data.Mashwiat_prise_1_2;
+    
     document.getElementById('Mashwiat_type_2').textContent = data.Mashwiat_type_2;
     document.getElementById('Mashwiat_prise_2').textContent = data.Mashwiat_prise_2;
+    document.getElementById('Mashwiat_type_2_1').textContent = data.Mashwiat_type_2_1;
+    document.getElementById('Mashwiat_prise_2_1').textContent = data.Mashwiat_prise_2_1;
+    document.getElementById('Mashwiat_type_2_2').textContent = data.Mashwiat_type_2_2;
+    document.getElementById('Mashwiat_prise_2_2').textContent = data.Mashwiat_prise_2_2;
+
     document.getElementById('Mashwiat_type_3').textContent = data.Mashwiat_type_3;
     document.getElementById('Mashwiat_prise_3').textContent = data.Mashwiat_prise_3;
+    document.getElementById('Mashwiat_type_3_1').textContent = data.Mashwiat_type_3_1;
+    document.getElementById('Mashwiat_prise_3_1').textContent = data.Mashwiat_prise_3_1;
+    document.getElementById('Mashwiat_type_3_2').textContent = data.Mashwiat_type_3_2;
+    document.getElementById('Mashwiat_prise_3_2').textContent = data.Mashwiat_prise_3_2;
+
     document.getElementById('Mashwiat_type_4').textContent = data.Mashwiat_type_4;
     document.getElementById('Mashwiat_prise_4').textContent = data.Mashwiat_prise_4;
+    document.getElementById('Mashwiat_type_4_1').textContent = data.Mashwiat_type_4_1;
+    document.getElementById('Mashwiat_prise_4_1').textContent = data.Mashwiat_prise_4_1;
+    document.getElementById('Mashwiat_type_4_2').textContent = data.Mashwiat_type_4_2;
+    document.getElementById('Mashwiat_prise_4_2').textContent = data.Mashwiat_prise_4_2;
+
     document.getElementById('Mashwiat_type_5').textContent = data.Mashwiat_type_5;
     document.getElementById('Mashwiat_prise_5').textContent = data.Mashwiat_prise_5;
+    document.getElementById('Mashwiat_type_5_1').textContent = data.Mashwiat_type_5_1;
+    document.getElementById('Mashwiat_prise_5_1').textContent = data.Mashwiat_prise_5_1;
+    document.getElementById('Mashwiat_type_5_2').textContent = data.Mashwiat_type_5_2;
+    document.getElementById('Mashwiat_prise_5_2').textContent = data.Mashwiat_prise_5_2;
+    
     document.getElementById('Mashwiat_type_6').textContent = data.Mashwiat_type_6;
     document.getElementById('Mashwiat_prise_6').textContent = data.Mashwiat_prise_6;
+    document.getElementById('Mashwiat_type_6_1').textContent = data.Mashwiat_type_6_1;
+    document.getElementById('Mashwiat_prise_6_1').textContent = data.Mashwiat_prise_6_1;
+    document.getElementById('Mashwiat_type_6_2').textContent = data.Mashwiat_type_6_2;
+    document.getElementById('Mashwiat_prise_6_2').textContent = data.Mashwiat_prise_6_2;
+
     document.getElementById('Mashwiat_type_7').textContent = data.Mashwiat_type_7;
     document.getElementById('Mashwiat_prise_7').textContent = data.Mashwiat_prise_7;
+    document.getElementById('Mashwiat_type_7_1').textContent = data.Mashwiat_type_7_1;
+    document.getElementById('Mashwiat_prise_7_1').textContent = data.Mashwiat_prise_7_1;
+    document.getElementById('Mashwiat_type_7_2').textContent = data.Mashwiat_type_7_2;
+    document.getElementById('Mashwiat_prise_7_2').textContent = data.Mashwiat_prise_7_2;
+
     document.getElementById('Mashwiat_type_8').textContent = data.Mashwiat_type_8;
     document.getElementById('Mashwiat_prise_8').textContent = data.Mashwiat_prise_8;
+    document.getElementById('Mashwiat_type_8_1').textContent = data.Mashwiat_type_8_1;
+    document.getElementById('Mashwiat_prise_8_1').textContent = data.Mashwiat_prise_8_1;
+    document.getElementById('Mashwiat_type_8_2').textContent = data.Mashwiat_type_8_2;
+    document.getElementById('Mashwiat_prise_8_2').textContent = data.Mashwiat_prise_8_2;
+
     document.getElementById('Mashwiat_type_9').textContent = data.Mashwiat_type_9;
     document.getElementById('Mashwiat_prise_9').textContent = data.Mashwiat_prise_9;
+    document.getElementById('Mashwiat_type_9_1').textContent = data.Mashwiat_type_9_1;
+    document.getElementById('Mashwiat_prise_9_1').textContent = data.Mashwiat_prise_9_1;
+    document.getElementById('Mashwiat_type_9_2').textContent = data.Mashwiat_type_9_2;
+    document.getElementById('Mashwiat_prise_9_2').textContent = data.Mashwiat_prise_9_2;
+
     document.getElementById('Mashwiat_type_10').textContent = data.Mashwiat_type_10;
     document.getElementById('Mashwiat_prise_10').textContent = data.Mashwiat_prise_10;
+    document.getElementById('Mashwiat_type_10_1').textContent = data.Mashwiat_type_10_1;
+    document.getElementById('Mashwiat_prise_10_1').textContent = data.Mashwiat_prise_10_1;
+    document.getElementById('Mashwiat_type_10_2').textContent = data.Mashwiat_type_10_2;
+    document.getElementById('Mashwiat_prise_10_2').textContent = data.Mashwiat_prise_10_2;
+    
     document.getElementById('Mashwiat_type_11').textContent = data.Mashwiat_type_11;
     document.getElementById('Mashwiat_prise_11').textContent = data.Mashwiat_prise_11;
     document.getElementById('Mashwiat_type_12').textContent = data.Mashwiat_type_12;
--- Back-end/Waraq_lahm.js
+++ Back-end/Waraq_lahm.js
@@ -6,5 +6,13 @@ fetch('./../../Data-base/Waraq_lahm.json')
     document.getElementById('Waraq_lahm_prise_1').textContent = data.Waraq_lahm_prise_1;
     document.getElementById('Waraq_lahm_type_2').textContent = data.Waraq_lahm_type_2;
     document.getElementById('Waraq_lahm_prise_2').textContent = data.Waraq_lahm_prise_2;
+    document.getElementById('Waraq_lahm_type_3').textContent = data.Waraq_lahm_type_3;
+    document.getElementById('Waraq_lahm_prise_3').textContent = data.Waraq_lahm_prise_3;
+    document.getElementById('Waraq_lahm_type_4').textContent = data.Waraq_lahm_type_4;
+    document.getElementById('Waraq_lahm_prise_4').textContent = data.Waraq_lahm_prise_4;
+    document.getElementById('Waraq_lahm_type_5').textContent = data.Waraq_lahm_type_5;
+    document.getElementById('Waraq_lahm_prise_5').textContent = data.Waraq_lahm_prise_5;
+    document.getElementById('Waraq_lahm_type_6').textContent = data.Waraq_lahm_type_6;
+    document.getElementById('Waraq_lahm_prise_6').textContent = data.Waraq_lahm_prise_6;
 
   });
